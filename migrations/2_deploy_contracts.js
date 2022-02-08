59f2fe74c6e230a982c19956c7f5d7a23a1ea547
sepehr <sph_1996@yahoo.com>
2022-02-08 16:38:34 +0330

    adding smart contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const NFT = artifacts.require("NFT");
+const NFTMarket = artifacts.require("NFTMarket");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+module.exports = async function (deployer) {
+  //deploy NFTMarket
+  await deployer.deploy(NFTMarket);
+  const nftMarket = await NFTMarket.deployed();
+  //deplo NFT
+  await deployer.deploy(NFT, nftMarket.address);
 };
